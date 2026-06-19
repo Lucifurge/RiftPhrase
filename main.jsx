c5416c0aa44054a830591d31d8cbe9006b9909f8
Lucifurge <tasogaresaya@gmail.com>
2026-06-20 06:04:39 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+
+import router from "./routes";
+
 import { AuthProvider } from "./contexts/AuthContext";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
+
     <AuthProvider>
+
       <RouterProvider router={router} />
+
     </AuthProvider>
+
   </React.StrictMode>
 );
